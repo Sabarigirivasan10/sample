5f2db1f0887008c1f4422279c18030697c75cb5d
sabari <sabari.mp777@gmail.com>
2025-07-23 17:46:27 +0530

    second

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(SYstem.in);
+        int n=sc.nextInt();
+        System.out.println(n+ " Printed Successfully");
+    }
+}

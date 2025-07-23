e86e6c1e9a70f40cc81415b21e3a6212af953bbe
sabari <sabari.mp777@gmail.com>
2025-07-23 17:48:40 +0530

    third

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main
         Scanner sc=new Scanner(SYstem.in);
         int n=sc.nextInt();
         System.out.println(n+ " Printed Successfully");
+        System.out.println(n+ " Printed Successfully");
     }
 }
